e2b3aa740bd9598ae1f59160c465a80f5d5b0c53
thangabalu <thangabalu@gmail.com>
2014-03-23 01:26:10 +0100

    minor

--- wsgi/surekha_app/settings.py
+++ wsgi/surekha_app/settings.py
@@ -159,7 +159,7 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
-    'openshift'
+    'surekha_app'
 )
 
 # A sample logging configuration. The only tangible logging
