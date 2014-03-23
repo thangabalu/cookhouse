d1893a537ab886cd5e6be3eeb657ea936e18b45f
thangabalu <thangabalu@gmail.com>
2014-03-23 12:37:56 +0100

    minor

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from setuptools import setup
 
-setup(name='surekhas',
+setup(
+      name='surekha_app',
       version='1.0',
       description='surekhas cook house',
       author='surekha',
       author_email='surekhascookhouse@gmail.com',
       url='http://www.python.org/sigs/distutils-sig/',
-      install_requires=['Django>=1.3'],
+      install_requires=['Django>=1.4'],
      )
