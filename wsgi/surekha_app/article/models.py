216ca7d1dd666d97cc9518e253b1be4aeb3c1123
thangabalu <thangabalu@gmail.com>
2014-05-03 03:57:00 +0200

    twenteeth push

---
+++ wsgi/surekha_app/article/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+import os
+import time
+# Create your models here.
+
+def get_upload_file_name(instance, filename):
+	return "uploaded_files/%s_%s" % (str(time.time()).replace('.','_'), filename)
+
+# I am making the title field unique, because I will use this in the part of the url
+class Article(models.Model):
+
+    Recipe_Choices = (
+      ('cake', 'cakes'),
+      ('quick', 'quick food'),
+      ('past', 'pastries'),
+      ('variety', 'variety rice'),
+      ('one', 'one pot cooking')
+    )
+    title           = models.CharField(max_length=200, unique = True)
+    ingredients     = models.TextField()
+    pub_date        = models.DateTimeField(auto_now=True)
+    directions      = models.TextField()
+    note            = models.TextField(blank = True)
+    tips            = models.TextField(blank = True)
+    photo           = models.FileField(upload_to= get_upload_file_name)
+    recipe_type     = models.CharField(max_length=10,choices=Recipe_Choices)
+
+    class Meta:
+        app_label = 'article'
