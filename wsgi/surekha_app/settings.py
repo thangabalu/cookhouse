1d5c778ff5c59811e0d700e5cd97d46cee78e439
thangabalu <thangabalu@gmail.com>
2014-05-03 21:12:55 +0200

    thirtieth push

--- wsgi/surekha_app/settings.py
+++ wsgi/surekha_app/settings.py
@@ -73,7 +73,8 @@ USE_L10N = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = os.environ.get('OPENSHIFT_DATA_DIR', 'media')
+#MEDIA_ROOT = os.environ.get('OPENSHIFT_DATA_DIR', 'media')
+MEDIA_ROOT: os.path.join(os.environ['OPENSHIFT_DATA_DIR'], 'media')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
