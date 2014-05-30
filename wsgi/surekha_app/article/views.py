5d844ae288adcd58710d614652a4681cba21d040
thangabalu <thangabalu@gmail.com>
2014-05-30 22:57:09 +0200

    changes in views and home

--- wsgi/surekha_app/article/views.py
+++ wsgi/surekha_app/article/views.py
@@ -8,8 +8,8 @@ from article.models import Article
 #Order.objects.order_by('-date')[0]
 def home(request):
    return render_to_response('home.html', 
-				{'latest_recipes_two' : Article.objects.all().order_by('-pub_date')[:2],
-                              	 'random_recipes_three' : Article.objects.all().order_by('?')[:3]})
+				{'latest_recipes_one' : Article.objects.all().order_by('-pub_date')[:1],
+                              	 'random_recipes_one' : Article.objects.all().order_by('?')[:1]})
 
 def recipetype (request, recipetype):
     return render_to_response('recipe_type_new.html',
