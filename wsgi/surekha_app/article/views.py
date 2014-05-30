8fea64f7c273f103981607db62355c93122cf793
thangabalu <thangabalu@gmail.com>
2014-05-30 21:55:41 +0200

    changes in views and home

--- wsgi/surekha_app/article/views.py
+++ wsgi/surekha_app/article/views.py
@@ -2,13 +2,13 @@ from django.shortcuts import HttpResponse
 from django.template.loader import get_template
 from django.template import Context
 from django.shortcuts import render_to_response
-from models import Article
+from article.models import Article
 
 # Create your views here.
 #Order.objects.order_by('-date')[0]
 def home(request):
    return render_to_response('home.html', 
-				{'latest_recipes_three' : Article.objects.all().order_by('-pub_date')[:3],
+				{'latest_recipes_two' : Article.objects.all().order_by('-pub_date')[:2],
                               	 'random_recipes_three' : Article.objects.all().order_by('?')[:3]})
 
 def recipetype (request, recipetype):
@@ -20,7 +20,7 @@ def showrecipe (request, recipetitle=""):
     return render_to_response('show_recipe.html',
 		{'article': Article.objects.get(title=recipetitle.replace("-"," ")),
 		 'latest_recipes_three' : Article.objects.all().order_by('-pub_date')[:5],
-		 'random_recipes_three' : Article.objects.all().order_by('?')[:5]
+		 'random_recipes_three' : Article.objects.all().order_by('?')[:5],
 		})
 
 def recipes_all(request):
