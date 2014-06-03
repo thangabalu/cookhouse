9ff5c69f1e941c655fe9773ede6477b62f5048c7
thangabalu <thangabalu@gmail.com>
2014-06-03 22:51:49 +0200

    adding urls.py

--- wsgi/surekha_app/article/urls.py
+++ wsgi/surekha_app/article/urls.py
@@ -5,5 +5,5 @@ urlpatterns =patterns('',
 	url(r'^all/$', 'article.views.recipes_all'),
 	# P  - passing a parameter
 	url(r'^(?P<recipetype>[-\w\d\s]+)/$', 'article.views.recipetype'),
-	url(r'^[\w]+/(?P<recipetitle>[-\w\d\s]+)/$', 'article.views.showrecipe'),
+	url(r'^[\w]+/(?P<recipetitle>[-\w\d\s()]+)/$', 'article.views.showrecipe'),
 )
